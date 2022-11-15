29a0617de6694cfacd27eedd95981a52831dc42a
BrankaKljajic <branka.kljajic00@gmail.com>
2022-11-16 00:46:47 +0100

    Finished UI for displaying blood centers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { HomePageModule } from './pages/home-page/home-page.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { HomePageModule } from './pages/home-page/home-page.module';
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
-    HomePageModule
+    HomePageModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
