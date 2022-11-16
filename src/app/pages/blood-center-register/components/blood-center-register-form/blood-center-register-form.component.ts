c1126948f17eccd6b283620785e7931e3f3de963
SaraJokic <94146847+SaraJokic@users.noreply.github.com>
2022-11-16 20:36:58 +0100

    Added small changes to the form

--- src/app/pages/blood-center-register/components/blood-center-register-form/blood-center-register-form.component.ts
+++ src/app/pages/blood-center-register/components/blood-center-register-form/blood-center-register-form.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { HomePageModule } from 'src/app/pages/home-page/home-page.module';
+import { BloodCenter } from 'src/app/pages/home-page/interfaces/BloodCenter';
+import { BloodBankService } from 'src/app/pages/home-page/services/blood-bank.service';
+
+
 
 @Component({
   selector: 'app-blood-center-register-form',
@@ -7,12 +13,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BloodCenterRegisterFormComponent implements OnInit {
 
-  constructor() { }
-  selected = ''
+  constructor(private bloodBankService: BloodBankService) { }
+  selected = '';
+  public bloodCenters: BloodCenter[] =[];
+  
+
   ngOnInit(): void {
+    this.bloodBankService.findAll().subscribe((res)=>{
+      this.bloodCenters = res;
+      console.log(res)
+     
+    }) 
   }
   getAdmin(admin : any){
     console.log(admin)
-
+    
   }
+  onSelected(a : any){
+    this.selected=a.value;
+    console.log(a)
+    
+  }
+
+  
+
 }
