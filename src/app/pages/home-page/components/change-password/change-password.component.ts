4b1c8eeff480e5fbe84e833fb069cb49b6dc4521
SaraJokic <94146847+SaraJokic@users.noreply.github.com>
2022-12-23 15:01:48 +0100

    added changes on change password

--- src/app/pages/home-page/components/change-password/change-password.component.ts
+++ src/app/pages/home-page/components/change-password/change-password.component.ts
@@ -1,4 +1,7 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/pages/user-profile/services/user.service';
+import { RegisterUserService } from '../../services/register-user.service';
 
 @Component({
   selector: 'app-change-password',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChangePasswordComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private userService : UserService,private registrationService: RegisterUserService) { }
+  public loggedUser : any
   ngOnInit(): void {
   }
-
+  changePassword(p : any)
+  {
+    
+    this.registrationService.getLoggedUser().subscribe((data)=>{
+      this.loggedUser=data;
+    });
+   
+    this.loggedUser.password=p;
+   
+    this.userService.updateUserInfo(this.loggedUser).subscribe(
+     /* (data) => {
+        alert("Success!");
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+ */
+    )}
 }
